f0ecf16b24c97ae3cc4f44be6518a284f4521b11
Mateusz Stradomski <mateusz.stradomski01@gmail.com>
2023-10-20 08:50:51 +0200

    Lab1

--- Lab 1 - rozgrzewka/main.js
+++ Lab 1 - rozgrzewka/main.js
@@ -1,9 +1,21 @@
-// notatnik z zajęć
-const liczba1 = document.querySelector('#liczba1')
-const btnPrzelicz = document.querySelector('#przelicz')
-const wynikiPojemnik = document.querySelector('#wyniki')
+function calculate(){
+    
 
-btnPrzelicz.addEventListener('click', () => {
-    wynikiPojemnik.innerHTML = liczba1.value
-    console.log(liczba1.value)
-})
+    let a = parseInt(document.querySelector("#a").value);
+    let b = parseInt(document.querySelector("#b").value);
+    let c = parseInt(document.querySelector("#c").value);
+    let d = parseInt(document.querySelector("#d").value);
+
+    let sum = a + b + c + d;
+    let sred = (a + b + c + d) / 4;
+    let min = Math.min(a, b, c, d);
+    let max = Math.max(a, b, c, d);
+
+    document.getElementById("sum").value = sum;
+    document.getElementById("sred").value = sred;
+    document.getElementById("min").value = min;
+    document.getElementById("max").value = max;
+}
+
+let inputs = document.querySelectorAll("input");
+inputs.forEach(input => input.addEventListener("change", calculate));
